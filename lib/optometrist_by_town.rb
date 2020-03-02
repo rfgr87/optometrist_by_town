906110955c71f0b14ffc20b6378f33278f625071
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-02 20:19:42 +0000

    first commit

---
+++ lib/optometrist_by_town.rb
@@ -0,0 +1,6 @@
+require "optometrist_by_town/version"
+
+module OptometristByTown
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/optometrist_by_town/version.rb
@@ -0,0 +1,3 @@
+module OptometristByTown
+  VERSION = "0.1.0"
+end
