8860409de260a68104b8dbd8b8df8991ce561016
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-03 16:18:15 +0000

    odified

--- lib/optometrist_by_town.rb
+++ lib/optometrist_by_town.rb
@@ -2,7 +2,21 @@ require_relative "optometrist_by_town/version"
 require_relative "./cli.rb"
 
  module OptometristByTown
-   def call
-     puts "Hello"
+   attr_accessor :town, :name, :number
+   @@all = []
+   
+   def initialize(town=nil, name=nil, number=nil, webpage=nil)
+     @town = town
+     @name = name 
+     @number = number
+     @webpage = webpage
+   end
+   
+   def self.save
+     @@all << self
+   end
+   
+   def self.all
+     @@all
    end
  end
