bcdf9befba8f83a3fc12858dc6ba5ba53c480e98
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-04 22:18:55 +0000

    modified find_office.rb

--- lib/find_office.rb
+++ lib/find_office.rb
@@ -17,20 +17,44 @@ class OptometristByTown::FindOffice
   
   #doc.css("div.container-fluid")[0].css(".col-md-4.col-xs-4.text-right")[0].text
   def find_office
-    binding.pry
     @doc.css("#main div.row").each do |elements|
+      
       elements.css("a").each do |link|
         if link.attr('href').to_s.include?(@town)
-          @office_url = link.attr('href').to_s
+          @office_url = link.attr('href')
+          @office_url = "http://www.opticaspr.com" + @office_url  
           @office_detail_page = Nokogiri::HTML(open(@office_url))
+          
           @office_detail_page.css("div.container-fluid div.row").each do |x|
-            name = x.css(".col-md-3.hidden-xs.hidden-sm.text-left a")[1].text
-            town = @town 
-            address = x.css(".col-md-4.col-xs.text-right").text
             
+
+#x.css("div.col-md-3.hidden-xs.hidden-sm.text-left")[0].css("a").css('img').map{ |i| i['alt'] }[0] 
+#x.css("div.col-md-3.hidden-xs.hidden-sm.text-left").css("a").css('img').map{ |i| i['alt'] }[0] 
+# search('div.consultant img').map{ |img| img['alt'] }
+            # NAME
+            name = x.css("div.col-md-3.hidden-xs.hidden-sm.text-left").css("a").css('img').map{ |i| i['alt'] }[0] 
+            
+            #TOWN
+            town = @town.capitalize 
+            
+            # ADDRESS
+            y = x.css("div.col-md-4.col-xs-4.text-right")
+            y.search("span").remove
+            address = y.text.strip
+            
+
             # FOR THE NUMBER WE HAVE TO GO TO ANOTHER PAGE...
+            number_link = x.css("div.col-md-5.col-xs-8 a")[1].attr('href')
+            number_link = "http://www.opticaspr.com" + number_link 
+            number_html = Nokogiri::HTML(open(number_link))
+            binding.pry
+            number = number_html.css("div.row i.fa.fa-phone").first
             
+            number_html.css("i.fa.fa-phone").text
+
             number = x.css( )
+            
+
             if !css.(  ) == false
               webpage = css.(   ).attr('href')
             end
